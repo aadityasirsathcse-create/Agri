95ad2313c899c2e9d85c16e4a045b2a15d835011
Aaditya Sirsath <aadityasirsath@asmadiya.com>
2025-10-07 13:31:07 +0530

    Removed QRScannerView component and updated QRTrackerReportProductScreen styles.

--- src/plugins/qr-tracker/screens/QRTrackerReportProductScreen.tsx
+++ src/plugins/qr-tracker/screens/QRTrackerReportProductScreen.tsx
@@ -104,7 +104,7 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     borderWidth: 1,
     borderColor: '#ddd',
-    textAlign: 'center',
+    textAlign: 'left',
   },
   orText: {
     textAlign: 'center',
